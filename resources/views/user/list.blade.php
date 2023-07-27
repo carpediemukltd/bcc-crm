2195b774e3f8b6d11ff06c52e78250fc518803c3
Wasiq <engr.mwansari@gmail.com>
2023-07-28 00:25:55 +0500

    fixed duplication of error message code all_type_alert_boxes

--- resources/views/user/list.blade.php
+++ resources/views/user/list.blade.php
@@ -32,29 +32,7 @@
 </div>
 <div class="content-inner pb-0 container-fluid" id="page_layout">
    <div>
-      <div class="all_type_alert_boxes">
-         @if (session('success'))
-         <div class="row">
-            <div class="col-md-12">
-               <div class="alert alert-success alert-dismissible fade show" role="alert">
-                  <strong>Congratulations!</strong> {{ session('success') }}
-                  <button type="button" class="btn-close" id="alert-box-close" data-bs-dismiss="alert" aria-label="Close"></button>
-               </div>
-            </div>
-         </div>
-         @endif
-
-         @if (session('error'))
-         <div class="row">
-            <div class="col-md-12">
-               <div class="alert alert-warning alert-dismissible fade show" role="alert">
-                  <strong>Oops!</strong> {{ session('error') }}
-                  <button type="button" class="btn-close" id="alert-box-close" data-bs-dismiss="alert" aria-label="Close"></button>
-               </div>
-            </div>
-         </div>
-         @endif
-      </div>
+      @include('alert_message')
       <div class="row">
          <div class="col-sm-12">
             <div class="card">

096e7af611fac02d44b405f8d17c934caade0e6e
Muhammad Junaid <muhammadjunaidswe@gmail.com>
2023-08-28 20:28:10 +0500

    DialogflowController.php | chatbot crm integration

--- app/Http/Controllers/DialogflowController.php
+++ app/Http/Controllers/DialogflowController.php
@@ -2,12 +2,54 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Deals;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class DialogflowController extends Controller
 {
-    public static function index(Request $request) : string
+    public static function index(Request $request) : array
     {
-        return $request->queryResult["queryText"];
+        try
+        {
+            $aParameterValue    = array_keys($request->queryResult["parameters"]);
+            $sParameterFunction = $aParameterValue[0];
+
+            $thisObj = new self;
+            if(method_exists($thisObj, $sParameterFunction))
+                return self::$sParameterFunction($request->queryResult["parameters"]);
+
+            throw new \Exception("Method not found");
+        }
+        catch(\Exception $e)
+        {
+            return ["FulfillmentText" => $e->getMessage()];
+        }
+    }
+
+    private static function BankEmailAddress($aRequestParameters) : array
+    {
+        try
+        {
+            $aUserExists = User::where("email", $aRequestParameters["BankEmailAddress"])->get()->first();
+            if(!$aUserExists)
+                throw new \Exception("Record not found");
+
+            $iRecordId  = $aUserExists->id;
+            $sFirstName = $aUserExists->first_name;
+            $sLastName  = $aUserExists->last_name;
+
+            $aApplicationStatus = Deals::getApplicationStatus($iRecordId);
+            if(sizeof($aApplicationStatus) <= 0)
+                throw new \Exception("Sorry, no open application found. For more details please email on mjunaud292@gmail.com");
+
+            $sMessage = "Dear $sFirstName $sLastName your application status is ".$aApplicationStatus[0]->title." stage";
+
+            return ["fulfillmentText" => $sMessage];
+        }
+        catch(\Exception $e)
+        {
+            return ["fulfillmentText" => $e->getMessage()];
+        }
     }
 }

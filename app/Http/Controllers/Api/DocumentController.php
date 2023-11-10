ec6d03b545f94359f2d4997ba9e1ada8138e024f
alimashooque <mashooqcs@gmail.com>
2023-11-11 00:06:57 +0500

    doc templates in api

--- app/Http/Controllers/Api/DocumentController.php
+++ app/Http/Controllers/Api/DocumentController.php
@@ -107,6 +107,7 @@ class DocumentController extends Controller
         $data['ocrolus_csv_path'] = $ocrolusCSV;
         $data['showMenu'] = 1;
         $data['ocrolusCSVPath'] = 1;
+        $data['sba_products_templates'] = array('Personal_Financial_Statement'=> env('APP_URL').'BCCUSA_PersonalFinancialStatement_2023.pdf');
         return ApiResponse::success($data, '', 200);
     }
 

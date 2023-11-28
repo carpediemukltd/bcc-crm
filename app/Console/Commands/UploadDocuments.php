98bd7b5ec626eabf14090cb7b7a84f85cda0b3dc
unknown <waleed.ahmed@by433.com>
2023-11-28 22:29:04 +0500

    added email and sms template for due date passed and due date

--- app/Console/Commands/UploadDocuments.php
+++ app/Console/Commands/UploadDocuments.php
@@ -6,6 +6,7 @@ use Carbon\Carbon;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\DB;
 use Mail;
+use Twilio\Rest\Client;
 
 class UploadDocuments extends Command
 {
@@ -42,15 +43,17 @@ class UploadDocuments extends Command
     {
         $data = DB::table('document_manager_user')
             ->where('document_uploaded', 0)
+            ->where('due_date', '!=', '')
             ->join('users', 'document_manager_user.user_id', '=', 'users.id')
             ->join('document_managers', 'document_manager_user.document_manager_id', '=', 'document_managers.id')
-            ->select('document_manager_user.user_id', 'users.first_name', 'users.email', 'document_managers.title', 'document_manager_user.due_date')
-            ->groupBy('document_manager_user.user_id', 'users.first_name', 'users.email', 'document_manager_user.due_date', 'document_managers.title')
+            ->select('document_manager_user.user_id', 'document_manager_user.due_date', 'users.first_name', 'users.email', 'users.phone_number', 'document_managers.title', 'document_manager_user.due_date')
+            ->groupBy('document_manager_user.user_id', 'document_manager_user.due_date', 'users.first_name', 'users.email', 'users.phone_number', 'document_manager_user.due_date', 'document_managers.title')
             ->get();
         $groupedResults = $data->groupBy('user_id');
         foreach ($groupedResults as $documents) {
-            $userEmail = $documents[0]->email; // Assuming email is the same for a user in all records
-            $userFirstName = $documents[0]->first_name; // Assuming first_name is the same for a user in all records
+            $userEmail = $documents[0]->email;
+            $userFirstName = $documents[0]->first_name;
+            $userPhoneNumber = $documents[0]->phone_number;
 
             $dueDate = Carbon::parse($documents[0]->due_date);
             $currentDate = Carbon::now();
@@ -59,32 +62,61 @@ class UploadDocuments extends Command
                 'email' => $userEmail
             ];
             $subject = '';
-            foreach ($documents as $index => $document){
+            foreach ($documents as $index => $document) {
                 $subject .= $document->title;
-                $subject .= $index +1 == count($documents) ? ' ' : ', ';
+                $subject .= $index + 1 == count($documents) ? ' ' : ', ';
             }
 
             $isDueDatePassed = $dueDate->isPast();
-            if($isDueDatePassed){
+
+            if ($isDueDatePassed) {
                 $daysDifference = $dueDate->diffInDays($currentDate);
-                $subject .= " is already due - $daysDifference days ago";
-                $email['subject'] = $subject;
-            }else{
+            } else {
                 $daysDifference = $currentDate->diffInDays($dueDate);
-                $subject .= " is nearly due - $daysDifference days to go";
-                $email['subject'] = $subject;
             }
 
-            if($daysDifference <= 7 && $daysDifference % 2 == 1) {
-                Mail::send('email.newRegistration', [
-                    'first_name' => $userFirstName,
-                    'documents' => $documents
-                ], function ($message) use ($email) {
-                    $message->to($email['email']);
-                    $message->subject($email['subject']);
-                });
+            $template = '';
+            $message = '';
+            if ($isDueDatePassed) {
+                if ($daysDifference == 0 || $daysDifference % 2 == 1) {
+                    $template = 'email.duedate.passed';
+                    $email['subject'] = "BCCUSA Documents Overdue by $daysDifference Days";
+                    $message = "Attention $userFirstName! The documents required for your bank financing application with BCCUSA are now overdue by $daysDifference days. Navigate https://dashboard.bccusa.com/ to access your bank portal to securely upload. For a more optimized mobile experience, download the IOS App or Android App. Reply Stop to opt out.";
+                }
+            } else {
+                if ($daysDifference <= 5 || $daysDifference % 2 == 1) {
+                    $template = 'email.duedate.notpassed';
+                    $email['subject'] = "BCCUSA Bank Financing: Documents Pending & Due in $daysDifference Days";
+                    $message = "Hi $userFirstName! The documents required for your bank financing application with BCCUSA are due in $daysDifference days. Navigate https://dashboard.bccusa.com/ to access your bank portal to securely upload. For a more optimized mobile experience, download the IOS App or Android App. Reply Stop to opt out.";
+                }
             }
+
+            $this->info($template);
+            $this->info($email['email']);
+            $this->info($message);
+
+            Mail::send($template, [
+                'first_name' => $userFirstName,
+                'documents' => $documents,
+                'days' => $daysDifference
+            ], function ($message) use ($email) {
+                $message->to($email['email']);
+                $message->subject($email['subject']);
+            });
+
+            $twilioPhoneNumber = env('TWILIO_NUMBER');
+            $twilioSid = env('TWILIO_SID');
+            $twilioToken = env('TWILIO_AUTH_TOKEN');
+            $client = new Client($twilioSid, $twilioToken);
+            // Remove spaces from the phone number
+            $toPhoneNumber = str_replace(' ', '', $userPhoneNumber);
+            $client->messages->create(
+                $toPhoneNumber,
+                [
+                    'from' => $twilioPhoneNumber,
+                    'body' => $message,
+                ]
+            );
         }
-
     }
 }

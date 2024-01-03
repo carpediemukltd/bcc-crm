475ced943de029484d04f636a34ed0fc6d9a3623
Muhammad Nouman Qureshi <noumanq05@gmail.com>
2024-01-04 00:04:50 +0500

    function added

--- resources/views/layout/themeFooter.blade.php
+++ resources/views/layout/themeFooter.blade.php
@@ -964,6 +964,64 @@
       style.type = 'text/css';
       style.innerHTML = '.iti__country-list { display: none !important; }';
       document.head.appendChild(style);
+
+      phoneNumberInput.blur(function(){
+           $("#errorSpan").html("");
+           var countryCode = selectedCountryCodeInput.val();
+           var inputNumber = countryCode+phoneNumberInput.val();
+           var pattern = /^[+]\d{11}$/;
+           var oldPhoneNumber = $("#old_phone_number").val();
+           var currentPhoneNumber = phoneNumberInput.val();
+
+           if (!pattern.test(inputNumber))
+           {
+               phoneNumberInput.focus();
+               $("#errorSpan").html("Invalid Format");
+               return false;
+           }
+           else
+               $("#errorSpan").html("");
+
+           var mobileVerificationCheckBox = ($("#mobileVerifiedCheckbox").is(':checked')) ? 1 : 0;
+           var mobileVerifiedRadioButton = $("input[name='mobileVerified']:checked").val();
+
+           if (!localStorage.getItem("mobileVerificationCheckBox"))
+               localStorage.setItem("mobileVerificationCheckBox",mobileVerificationCheckBox);
+           if (!localStorage.getItem("mobileVerifiedRadioButton"))
+               localStorage.setItem("mobileVerifiedRadioButton",mobileVerifiedRadioButton);
+
+           if (oldPhoneNumber)
+           {
+               if (currentPhoneNumber != oldPhoneNumber)
+               {
+                   Swal.fire({
+                       icon: "error",
+                       title: "Error!",
+                       text: "You have changed your phone number now you have to verify it again."
+                   });
+
+                   if (mobileVerificationCheckBox == 1 || mobileVerifiedRadioButton == 1)
+                   {
+                       $("#mobileVerifiedCheckbox").prop("checked",false);
+                       $("input[data-id='notVerified']").prop("checked",true);
+                   }
+               }
+               else
+               {
+                   mobileVerificationCheckBox = localStorage.getItem("mobileVerificationCheckBox");
+                   mobileVerifiedRadioButton = localStorage.getItem("mobileVerifiedRadioButton");
+                   if (mobileVerificationCheckBox == 1)
+                       $("#mobileVerifiedCheckbox").prop("checked",true);
+                   else
+                       $("#mobileVerifiedCheckbox").prop("checked",false);
+                   if (mobileVerifiedRadioButton == 1)
+                       $("#mobileVerified").prop("checked",true);
+                   else
+                       $("input[data-id='notVerified']").prop("checked",true);
+
+               }
+           }
+       });
    });
 </script>
 <script>
